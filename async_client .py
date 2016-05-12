ed459ae681e110182c8025d9c00d2ee63444feb0
eteamin <aminetesamian1371@gmail.com>
2016-05-12 13:17:03 +0430

    fix

--- async_client .py
+++ async_client .py
@@ -4,14 +4,12 @@ from datetime import datetime, timedelta
 import requests
 
 
-
 async def get_async():
-    async with requests.get("url") as r:
-        return await r.text()
+    async with requests.get("http://www.varzesh3.com") as r:
+        return await r.content
 
 
 async def main():
-
 
     captured_time = datetime.now()
     result = await asyncio.gather(*[asyncio.Task(get_async()) for u in range(1, 100)], return_exceptions=True)
@@ -19,9 +17,7 @@ async def main():
     # result = [requests.get(u).content for u in urls]
 
     total_time = (datetime.now() - captured_time)
-    for r in result:
-        print(r[:100])
-    print("Total time: %ss" % total_time.seconds)
+    print("Total time: %ss" % (total_time))
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
